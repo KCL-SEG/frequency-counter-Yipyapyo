d40547c5599d1698e9e6475147de294733746006
Francis Yip <77790158+Yipyapyo@users.noreply.github.com>
2022-10-17 13:05:48 +0100

    change {} to []

--- frequencies.py
+++ frequencies.py
@@ -6,7 +6,7 @@ def frequencies(itemsIn):
     frequencies = {}
     for item in items:
         if item in frequencies:
-            frequencies{item} += 1
+            frequencies[item] += 1
         else:
             frequencies[item] = 1
     return frequencies
