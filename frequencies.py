2ad51d5f1075f6aed6a9db8a3154c4a8acfd95d3
Francis Yip <77790158+Yipyapyo@users.noreply.github.com>
2022-10-17 13:00:44 +0100

    updated frequencies method
    fixed the method

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,9 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        if item in frequencies:
+            frequencies{item} += 1
+        else:
+            frequencies[item] = 1
     return frequencies
