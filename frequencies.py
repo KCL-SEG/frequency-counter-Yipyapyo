218fa7871b35b9c8f02ba05c348c91a11dd4675f
Francis Yip <francisyip7@gmail.com>
2022-10-18 22:42:35 +0100

    fully working frequencies method

--- frequencies.py
+++ frequencies.py
@@ -2,11 +2,12 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(itemsIn):
-    items = itemsIn
+    items = map(str, itemsIn)
     frequencies = {}
     for item in items:
         if item in frequencies:
             frequencies[item] += 1
         else:
             frequencies[item] = 1
+    
     return frequencies
